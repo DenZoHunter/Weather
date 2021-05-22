22476b12a6d2c3249ef7f75159758a1a66ff9266
Denis <denzohunter@bk.ru>
2021-05-22 13:50:34 +0300

    add button and setOnClickListener

--- app/src/main/java/ru/geekbrains/weather/MainActivity.kt
+++ app/src/main/java/ru/geekbrains/weather/MainActivity.kt
@@ -1,11 +1,21 @@
 package ru.geekbrains.weather
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val button: Button = findViewById(R.id.click_btn)
+
+        button.setOnClickListener {
+            Toast.makeText(applicationContext, "Click!", Toast.LENGTH_SHORT).show()
+        }
     }
 }
